584a7bae41d25ec99228a9acb0e492c4f96706b8
Hakibara <gumgumedgar@gmail.com>
2025-03-29 23:03:33 +0100

    Changements variables et returns

--- Cryptography.py
+++ Cryptography.py
@@ -6,31 +6,27 @@ def create_key():
     """ Creates a random key 16 bytes long"""
     return os.urandom(16)
 
-def encrypt_file(key, input_file, encrypted_file):
+def encrypt_file(key, input_file):
     """Encrypts input_file with AES-256 CBC mode
     Args:
-    input_file is a .txt file 
-    encrypted_file is a .txt file filled with bytes"""
+    input_file is a .txt file"""
     cipher = AES.new(key, AES.MODE_CBC)
     iv = cipher.iv  # Generate IV
     with open(input_file, 'rb') as f:
         data = f.read()
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
-    with open(encrypted_file, 'wb') as f:
-        f.write(iv + encrypted_data)  # Save IV + ciphertext
+    return encrypted_data
 
-def decrypt_file(key, encrypted_file, output_file):
+def decrypt_file(key, encrypted_data):
     """Decrypts encrypted_file with AES-256 CBC mode
     Args:
-    encrypted_file is a .txt file filled with bytes, an encrypted file
-    output_file is a .txt fiel with the decrypted data"""
+    encrypted_data is a list filled with bytes"""
     with open(encrypted_file, 'rb') as f:
         iv = f.read(16)  # Read the first 16 bytes (IV)
         encrypted_data = f.read()
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-    with open(output_file, 'wb') as f:
-        f.write(decrypted_data)
+    return decrypted_data
     
 if __name__ == "__main__":
     key = create_key()
