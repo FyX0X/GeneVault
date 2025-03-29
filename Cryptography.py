8b4388558b6d1916d728e8f4c5b085762451d36a
Hakibara <gumgumedgar@gmail.com>
2025-03-29 23:13:01 +0100

    Good changes with good returns

--- Cryptography.py
+++ Cryptography.py
@@ -15,20 +15,20 @@ def encrypt_file(key, input_file):
     with open(input_file, 'rb') as f:
         data = f.read()
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
+    encrypted_data = iv + encrypted_data
     return encrypted_data
 
 def decrypt_file(key, encrypted_data):
     """Decrypts encrypted_file with AES-256 CBC mode
     Args:
     encrypted_data is a list filled with bytes"""
-    with open(encrypted_file, 'rb') as f:
-        iv = f.read(16)  # Read the first 16 bytes (IV)
-        encrypted_data = f.read()
+    iv = encrypted_data[:16]
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-    decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+    decrypted_data = unpad(cipher.decrypt(encrypted_data[16:]), AES.block_size)
     return decrypted_data
     
 if __name__ == "__main__":
     key = create_key()
-    encrypt_file(key, 'File.txt', 'EncryptedFile.txt')
-    decrypt_file(key, 'EncryptedFile.txt', 'DecryptedFile.xt')
+    liste=decrypt_file(key, encrypt_file(key, 'File.txt'))
+    with open("Test.txt", 'wb') as f:
+        f.write(liste)
