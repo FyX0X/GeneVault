fe4a896625f0dec0ce45ef11f87bec0b9170b294
Martin Ergo <57560166+FyX0X@users.noreply.github.com>
2025-03-30 00:33:11 +0100

    Update writing.py

--- writing.py
+++ writing.py
@@ -1,6 +1,5 @@
 import translator
 import reedsolo
-import zlib
 
 
 DATA_SIZE = 27  # 108 nt (204 bits) long (27 bytes)
@@ -14,10 +13,10 @@ def write_dna_strand(owner_id: int, file_id: int, index: int, data: bytes) -> st
         owner_id (int): The ID of the owner.
         file_id (int): The ID of the file.
         index (int): The index of the data.
-        data (bytes): The data to be written (must be 80 nt (160 bits)).
+        data (bytes): The data to be written (must be 108 nt).
     """
     # Check if the data is 100 nt (200 bits) long
-    assert len(data) == DATA_SIZE, "Data must be 80 nt (160 bits) long."
+    assert len(data) == DATA_SIZE, "Data must be 108 nt (27 bytes) long."
     assert isinstance(data, bytes), "Data must be of type bytes."
     assert isinstance(owner_id, int), "Owner ID must be of type int."
     assert isinstance(file_id, int), "File ID must be of type int."
@@ -26,12 +25,17 @@ def write_dna_strand(owner_id: int, file_id: int, index: int, data: bytes) -> st
     assert 0 <= file_id <= 65535, "File ID must be between 0 and 65535."
     assert 0 <= index <= 65535, "Index must be between 0 and 65535."
 
-    adn_str = prefix(owner_id, file_id, index)
+    owner_bytes = owner_id.to_bytes(2, byteorder="big", signed=False)  # 2 bytes (4 pairs)
+    file_bytes = file_id.to_bytes(2, byteorder="big", signed=False)  # 2 bytes (4 pairs)
+    index_bytes = index.to_bytes(2, byteorder="big", signed=False)  # 2 bytes (4 pairs)
+
+
+    adn_str = prefix(owner_bytes, file_bytes, index_bytes)
 
     rs_encoded = reedsolo_encode(data)
     adn_str += translator.bytes_to_dna(rs_encoded)  # Add Reed-Solomon encoded data to the ADN string
 
-    cs = checksum_crc32(rs_encoded)  # Calculate checksum of the data and ECC
+    cs = checksum_prime(owner_bytes, file_bytes, index_bytes, rs_encoded)  # Calculate checksum of the data and ECC
     adn_str += translator.bytes_to_dna(cs)  # Add checksum to the ADN string
 
     adn_str += suffix()  # Add suffix to the ADN string
@@ -39,13 +43,13 @@ def write_dna_strand(owner_id: int, file_id: int, index: int, data: bytes) -> st
     return adn_str
 
 
-def prefix(owner_id: int, file_id: int, index: int) -> str:
+def prefix(owner_id: bytes, file_id: bytes, index: bytes) -> str:
     """ Creates the prefix for the ADN string.
     The prefix consists of a fixed header and the owner ID, file ID, and index."""
     prefix = "ACAC" # start 4 pairs (1 byte)
-    prefix += translator.bytes_to_dna(owner_id.to_bytes(2, byteorder="big", signed=False)) # owner id 2 bytes
-    prefix += translator.bytes_to_dna(file_id.to_bytes(2, byteorder="big", signed=False)) # file id 2 bytes
-    prefix += translator.bytes_to_dna(index.to_bytes(2, byteorder="big", signed=False)) # index 2 bytes
+    prefix += translator.bytes_to_dna(owner_id) # owner id 2 bytes
+    prefix += translator.bytes_to_dna(file_id) # file id 2 bytes
+    prefix += translator.bytes_to_dna(index) # index 2 bytes
     return prefix
 
 
@@ -65,26 +69,26 @@ def reedsolo_encode(data: bytes) -> str:
     # Encode data using Reed-Solomon
     encoded_data = rs.encode(data)
 
-    print("len encoded_data:", len(encoded_data))
-    print("len(data):", len(data))
 
     return encoded_data
 
 
-def checksum_crc32(data: bytes) -> bytes:
-    """ Calculate the CRC32 checksum of the data. and truncaste it to 2 bytes."""
-
-    cs = (zlib.crc32(data) & 0xFFFF).to_bytes(2, byteorder="big", signed=False)  # 2 bytes (8 pairs)
-    print("len cs:", len(cs))
-    print("cs:", cs)
-    return cs
+def checksum_prime(owner: bytes, file: bytes, index: bytes, encoded_data: bytes) -> bytes:
+    """Calculate the checksum by dividing the data by the largest prime that fits in 2 bytes."""
+    LARGEST_PRIME = 65521  # Largest prime number that fits in 2 bytes
+    data = owner + file + index + encoded_data
+    print("checksum data:", data)
+    
+    # Calculate the checksum as the remainder of the sum of the data divided by the prime
+    checksum_value = sum(data) % LARGEST_PRIME
+    return checksum_value.to_bytes(2, byteorder="big", signed=False)
 
 
 if __name__ == "__main__":
     # Example usage
     owner_id = 12345
     file_id = 123
-    index = 1
+    index = 0
     data = b"Hello, World!"
     padded = data.ljust(DATA_SIZE, b'\x00')  # Pad the data to 80 nt (160 bits)
 
