c26474d012427a90ccf540fa5362574146097eda
Androbi <robin.andre.gouttebaron@gmail.com>
2025-03-30 05:01:47 +0200

    Update sliceur function to use custom end sequence for padding

--- sliceur.py
+++ sliceur.py
@@ -1,4 +1,6 @@
-def sliceur(data, n=10):
+end_sequence = b'\x4E' + b'\x45' + b'\x52' + b'\x4D'
+
+def sliceur(data, n):
     long = len(data)
     nslice = (long + n - 1) // n  
 
@@ -9,7 +11,17 @@ def sliceur(data, n=10):
         segment = data[start:end]
 
         if len(segment) < n:
-            segment += b'\x00' * (n - len(segment))
+            padding_required = n - len(segment)
+            segment += end_sequence[:min(padding_required, len(end_sequence))]
+            if len(segment) <= n:
+                # If the segment is still not long enough, fill with zeros
+                segment += b'\x00' * n - len(segment)
+                slices.append(segment)
+                return slices
+            slices.append(segment)
+            segment = end_sequence[padding_required:]
+            segment += b'\x00' * n - len(segment)
+
 
         slices.append(segment)
 
