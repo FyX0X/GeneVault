b12c287914fa990e0305e53a9765f654c2fb477d
Hakibara <gumgumedgar@gmail.com>
2025-03-30 08:24:28 +0200

    Handling errors

--- SERVER/ServerSide.py
+++ SERVER/ServerSide.py
@@ -13,6 +13,7 @@ PORT = 54321
 
 tokens = {}
 file_count = {}
+
 def reload_csv():
     with open("SERVER/data.csv", "r") as file:
         """Fill the tokens and file_count with the exixting data"""
@@ -43,38 +44,49 @@ def handle_client(client_socket):
         pass
 
     if action == "read":
-        if check_token(owner_id,token) is True:
-            file = "SERVER/client_data/" + str(owner_id) + "/" + str(packet[3]) + ".dna"
-            with open(file, 'r') as f:
-                reponse = f.read()    
-            client_socket.sendall(str(len(reponse)).encode()) 
-            Confirmation = client_socket.recv(1000000).decode()
-            if Confirmation == "Ok":
-                client_socket.sendall(reponse.encode())
-            print("reading")
+        try:
+            if check_token(owner_id,token) is True:
+                file = "SERVER/client_data/" + str(owner_id) + "/" + str(packet[3]) + ".dna"
+                with open(file, 'r') as f:
+                    reponse = f.read()    
+                client_socket.sendall(str(len(reponse)).encode()) 
+                Confirmation = client_socket.recv(1000000).decode()
+                if Confirmation == "Ok":
+                    client_socket.sendall(reponse.encode())
+                print("reading")
+        except:
+            client_socket.sendall(str("There has been an error in reading the file").encode())
     
     elif action == "write":
-        if check_token(packet[1],packet[2]) is True:
-            file_count[owner_id] += 1
-            reponse = str(file_count[owner_id])
-            client_socket.sendall(str(file_count[owner_id]).encode())
-            file_path = f"SERVER/client_data/{owner_id}/{file_count[owner_id]}.dna"
-            file_data = client_socket.recv(1000000).decode()
-            os.makedirs("SERVER/client_data/" + str(owner_id),exist_ok=True)
-            with open(file_path, 'w') as f:
-                f.write(file_data)
-            confirmation = "Ok"
-            client_socket.sendall(confirmation.encode())
-            print("writing")
+        try: 
+            if check_token(owner_id,token) is True:
+                file_count[owner_id] += 1
+                reponse = str(file_count[owner_id])
+                client_socket.sendall(str(file_count[owner_id]).encode())
+                file_path = f"SERVER/client_data/{owner_id}/{file_count[owner_id]}.dna"
+                file_data = client_socket.recv(1000000).decode()
+                os.makedirs("SERVER/client_data/" + str(owner_id),exist_ok=True)
+                with open(file_path, 'w') as f:
+                    f.write(file_data)
+                confirmation = "Ok"
+                client_socket.sendall(confirmation.encode())
+                print("writing")
+        except:
+            client_socket.sendall(str("There has been an error in writing the file").encode())
+
     
     elif packet[0] == "register":
-        print('Registering')
-        with open("SERVER/data.csv", "a") as f:
-            owner_id = len(tokens)
-            hash_token = hashlib.sha256(token.encode()).hexdigest()
-            f.write(str(owner_id)+ "," + str(hash_token) + ",0 \n")
-        client_socket.sendall(str(owner_id).encode())
-        reload_csv()
+        try:
+            print('Registering')
+            with open("SERVER/data.csv", "a") as f:
+                owner_id = len(tokens)
+                hash_token = hashlib.sha256(token.encode()).hexdigest()
+                f.write(str(owner_id)+ "," + str(hash_token) + ",0 \n")
+            client_socket.sendall(str(owner_id).encode())
+            reload_csv()
+        except:
+            client_socket.sendall(str("There has been an error in registering").encode())
+
 
 
 def main():
@@ -90,8 +102,6 @@ def main():
         handle_client(client_socket)
 
     client_socket.close()
-    
-    
 
 if __name__ == "__main__":
     main()
